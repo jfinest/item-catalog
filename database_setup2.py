17e69457a3a165b73992a046524fcfaac8e8859b
JosueAcosta <josueacosta@IsidroskBookPro.home>
2017-05-01 20:21:24 -0400

    First commit of project.

---
+++ database_setup2.py
@@ -0,0 +1,58 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250), nullable=True)
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+    	"""Return object data in easily serializeable format"""
+    	return {
+    		'name': self.name,
+    		'id': self.id,
+    	}
+
+class Book(Base):
+	__tablename__ = 'book'
+
+	id = Column(Integer, primary_key=True)
+	title = Column(String(250), nullable=False)
+	author = Column(String(250), nullable=False)
+	description = Column(String(2000), nullable=False)
+	category_name = Column(String(250), ForeignKey('category.name'))
+	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		"""Return object data in easliy serializeable format"""
+		return {
+			'title': self.title,
+			'author': self.author,
+			'description': self.description,
+			'category_name': self.category_name,
+			'id': self.id,
+		}
+
+engine = create_engine('sqlite:///books.db')
+
+
+Base.metadata.create_all(engine)
---
+++ project2.py
@@ -0,0 +1,240 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup2 import Base, Category, Book, User
+from random import randint
+
+#Importing for login_session
+from flask import session as login_session
+import random, string
+
+#importing for OAuth
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///books.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+#route for login
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    #print "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+	#disconnecting user
+	credentials = login_session.get('credentials')
+	if credentials is None:
+		response = male_response(json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	#revoking current token 
+	access_token = credentials.access_token
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' %access_token
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+	if result['status'] == '200':
+		del login_session['credentials']
+		del login_session['gplus_id']
+		del login_session['username']
+		del login_session['email']
+		del login_session['picture']
+
+		response = make_response(json.dumps('Successfully disconnected'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	else:
+		response = make_response( json.dumps('Failed to revoke token for a given user.'), 400)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+@app.route('/')
+@app.route('/book/')
+def bookList():
+  categories = session.query(Category).all()
+
+  return render_template('booklist.html', categories=categories)
+
+# @app.route('/book/<category_name>/JSON')
+# def categoryListJSON(category_name):
+# 	selectedCategory = session.query(Book).filter_by(category_name=category_name).all()
+# 	return jsonify(BookLists=[i.serialize for i in selectedCategory])
+
+# @app.route('/book/<category_name>/<book_view>/JSON')
+# def selectedBookJSON(category_name, book_view):
+# 	selectedBook = session.query(Book).filter_by(title=book_view).one()
+# 	return jsonify(BookSelected=selectedBook.serialize)
+
+@app.route('/book/<category_name>/')
+def selectedCategoryList(category_name):
+  # name = session.query(Category).filter_by(id=categoryId).one()
+  selectedCategory = session.query(Book).filter_by(category_name=category_name).all()
+
+  return render_template('categorylist.html', selectedCategory=selectedCategory)
+
+
+@app.route('/book/<category_name>/<book_view>/')
+def viewSelectedBook(category_name, book_view):
+  bookSelected = session.query(Book).filter_by(title=book_view).one()
+
+  return render_template('bookView.html', bookSelected=bookSelected)
+
+@app.route('/book/new/', methods=['GET', 'POST'])
+def newBook():
+  # if 'username' not in login_session:
+  #   return redirect('/login')
+  if request.method == 'POST':
+  	number = randint(6,12500)
+  	newbook = Book(title=request.form['title'], author=request.form['author'], description=request.form['description'], category_name=request.form['category'])
+  	session.add(newbook)
+  	# flash('New Book %s Successfully Created' % newBook.title)
+  	session.commit()
+
+	return redirect(url_for('bookList'))
+
+  else:
+  	return render_template('newbook.html')
+
+@app.route('/book/<book_for_edit>/edit/', methods=['GET', 'POST'])
+def editBook(book_for_edit):
+	# if 'username' not in login_session:
+	# 	return redirect('/login')
+	editedBook = session.query(Book).filter_by(title=book_for_edit).one()
+	if request.method == 'POST':
+		if request.form['title']:
+			editedBook.title = request.form['title']
+		if request.form['author']:
+			editedBook.author = request.form['author']
+		if request.form['description']:
+			editedBook.description = request.form['description']
+		if request.form['category']:
+			editedBook.category_name = request.form['category']
+		session.add(editedBook)
+		# flash('Book Successfully Edited')
+		session.commit()
+		return redirect(url_for('bookList'))
+
+	else:
+	  return render_template(
+          'editBook.html', editedBook=editedBook)
+
+
+@app.route('/book/<book_to_delete>/delete/', methods=['GET', 'POST'])
+def deleteBook(book_to_delete):
+	# if 'username' not in login_session:
+	# 	return redirect('/login')
+	deletingBook = session.query(Book).filter_by(title=book_to_delete).first()
+	if request.method == 'POST':
+		session.delete(deletingBook)
+		# flash('%s Successfully Deleted' % deletingBook.name)
+		session.commit()
+		return redirect(url_for('bookList'))
+	else:
+		return render_template('deletebook.html', book_to_delete=deletingBook)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.secret_key = 'super_secret_key'
+    app.run(host='0.0.0.0', port=5000)
